faada0b6f34abdc9a80e50d95eeda3a8f9627f45
krt-n201x <kitsada_t@cmu.ac.th>
2021-08-10 15:15:30 +0700

    Add component
    to manage each patient information

--- project-vue/src/router/index.js
+++ project-vue/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Vaccine from '@/views/event/Vaccine.vue'
+import DoctorComment from '@/views/event/DoctorComment.vue'
+import Details from '@/views/event/Details.vue'
+import EventLayout from '@/views/event/Layout.vue'
 
 const routes = [
   {
@@ -16,6 +20,31 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: '/event/:id',
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'Details',
+        component: Details
+      },
+      {
+        path: 'doctorcomment',
+        name: 'DoctorComment',
+        props: true,
+        component: DoctorComment
+      },
+      {
+        path: 'vaccine',
+        name: 'Vaccine',
+        props: true,
+        component: Vaccine
+      }
+    ]
+  }
 ];
 
 const router = createRouter({
---
+++ project-vue/src/views/event/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="GStore.event">
+    <div id="nav">
+      <router-link :to="{ name: 'Details' }">Details</router-link>
+      |
+      <router-link :to="{ name: 'Vaccine' }">Vaccine</router-link>
+      |
+      <router-link :to="{ name: 'DoctorComment' }">DoctorComment</router-link>
+    </div>
+    <router-view :event="GStore.event" />
+  </div>
+</template>
