049a092d5c3b7da6015ef486699b5c9d7c2c288c
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-08-10 15:44:09 +0700

    add some code in PatientCard

--- project-vue/src/views/Home.vue
+++ project-vue/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
+  <div class="patient">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <div v-for="patient in patients" :key="patient.id" :patient="patient">
-      {{ patient.name }} {{ patient.surname }}
+    <PatientCard v-for="patient in patients" :key="patient.id" :patient="patient"/>
+      <!-- {{ patient.name }} {{ patient.surname }} -->
     </div>
-  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DatabaseService from "@/services/DatabaseService.js";
+import PatientCard from '../components/PatientCard.vue';
 
 export default {
+  components: { PatientCard },
   name: "PatientList",
   data(){
     return {
@@ -20,7 +21,7 @@ export default {
   },
   created(){
     DatabaseService.getAllPatients()
-       .then((response) => {
+      .then((response) => {
         this.patients = response.data
       })
       .catch((error) => {
