e325e7f4ded674dbebacfa13056976670980ec1a
krt-n201x <kitsada_t@cmu.ac.th>
2021-08-11 00:04:03 +0700

    Add next page fucntion
    Seperate patients 8 people/page

--- project-vue/src/router/index.js
+++ project-vue/src/router/index.js
@@ -10,15 +10,13 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import("../views/About.vue"),
   },
   {
     path: '/event/:id',
--- project-vue/src/views/Home.vue
+++ project-vue/src/views/Home.vue
@@ -1,32 +1,104 @@
 <template>
+<div>
   <div class="patient">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <PatientCard v-for="patient in patients" :key="patient.id" :patient="patient"/>
-      <!-- {{ patient.name }} {{ patient.surname }} -->
+  
+
+    <div class="pagination">
+      <router-link
+        id="back"
+        :to="{ name: 'Home', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        Back</router-link
+      >
+      <router-link
+        id="next"
+        :to="{ name: 'Home', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next</router-link
+      >
     </div>
+  </div>
+</div>
+
 </template>
 
 <script>
-// @ is an alias to /src
 import DatabaseService from "@/services/DatabaseService.js";
 import PatientCard from '../components/PatientCard.vue';
 
 export default {
   components: { PatientCard },
   name: "PatientList",
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
   data(){
     return {
       patients: null,
+      totalEvents: 0
     }
   },
-  created(){
-    DatabaseService.getAllPatients()
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    DatabaseService.getPatients(8, parseInt(routeTo.query.page) || 1)
       .then((response) => {
-        this.patients = response.data
+        next((comp) => {
+          comp.patients = response.data
+          comp.totalEvents = response.headers['x-total-count']
+        })
       })
       .catch((error) => {
         console.log(error)
       })
+  },
+  beforeRouteUpdate(routeTo) {
+    DatabaseService.getPatients(8, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        this.patients = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 8)
+      return this.page < totalPages
+    }
   }
 };
 </script>
+
+<style scoped>
+
+.patient {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#back {
+  text-align: left;
+}
+
+#next {
+  text-align: right;
+}
+</style>
