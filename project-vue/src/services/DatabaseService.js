5e14d32fb9dd8d958721f5214981da31b469ecf3
unknown <peerapat.next@gmail.com>
2021-08-12 18:47:25 +0700

    deploy prep

--- project-vue/src/services/DatabaseService.js
+++ project-vue/src/services/DatabaseService.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3004',
-  withCredentials: false,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
+    baseURL: process.env.VUE_APP_BACKEND_URL,
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
 })
 
 export default {
-  getAllPatients() {
-    return apiClient.get('/patients')
-  },
-  getPatients(perPage, page) {
-    return apiClient.get('/patients?_limit=' + perPage + '&_page=' + page)
-  },
-  //Added new call
-  getPatient(id) {
-    return apiClient.get('/patients/' + id)
-  }
+    getAllPatients() {
+        return apiClient.get('/patients')
+    },
+    getPatients(perPage, page) {
+        return apiClient.get('/patients?_limit=' + perPage + '&_page=' + page)
+    },
+    //Added new call
+    getPatient(id) {
+        return apiClient.get('/patients/' + id)
+    }
 }
