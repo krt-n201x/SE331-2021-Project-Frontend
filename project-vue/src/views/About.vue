f79e95fcb259e52f72270f555a486effec354db5
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-08-14 12:36:28 +0700

    Update about page

--- project-vue/src/views/About.vue
+++ project-vue/src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="FS">
+  <div class="size">
+<Fieldset legend="About">
+  <div class="text">
+  <h1>953331</h1>
+  <h1>Component-Based Software Develoment</h1>
+  <p>622115001 KANOKNART CHAWKANHA</p>
+  <p>622115003 KRITSADA THATNGOENKWEIN</p>
+  <p>622115029 PEERAPAT THUNGNGOEN</p>
   </div>
+</Fieldset>
+  </div>
+</div>
 </template>
+
+
+<script>
+export default {
+  props: ['patients'],
+}
+</script>
+
+<style>
+.FS{
+  text-align: left;
+}
+
+.photo {
+  width: 250px;
+  padding-left: 25%;
+}
+
+.size{
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+.text{
+  text-align: center;
+}
+
+</style>
