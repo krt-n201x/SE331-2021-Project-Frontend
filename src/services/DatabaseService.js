c896e79ad29ec62fa0bb1fe289e7bf8ef684fd00
Peerapat <peerapat_th@cmu.ac.th>
2021-10-27 05:20:04 +0700

    add Vaccine

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -1,47 +1,50 @@
 import apiClient from '@/services/AxiosClient.js'
 
 export default {
-  getAllPatients() {
-    return apiClient.get('/patients')
-  },
-  getAllDoctors() {
-    return apiClient.get('/doctors')
-  },
-  getPatients(perPage, page) {
-    return apiClient.get('/patients?_limit=' + perPage + '&_page=' + page)
-  },
-  //Added new call
-  getPatient(id) {
-    return apiClient.get('/patients/' + id)
-  },
-  getUsers() {
-    return apiClient.get('/admin')
-  },
-  getVaccines() {
-    return apiClient.get('/vaccines')
-  },
-  saveDoctor(user) {
-    return apiClient.post('/doctors', {
-      name: user.firstname,
-      surname: user.lastname
-    })
-  },
-  saveComment(patient, id) {
+    getAllPatients() {
+        return apiClient.get('/patients')
+    },
+    getAllDoctors() {
+        return apiClient.get('/doctors')
+    },
+    getPatients(perPage, page) {
+        return apiClient.get('/patients?_limit=' + perPage + '&_page=' + page)
+    },
+    //Added new call
+    getPatient(id) {
+        return apiClient.get('/patients/' + id)
+    },
+    getUsers() {
+        return apiClient.get('/admin')
+    },
+    getVaccines() {
+        return apiClient.get('/vaccines')
+    },
+    saveDoctor(user) {
+        return apiClient.post('/doctors', {
+            name: user.firstname,
+            surname: user.lastname
+        })
+    },
+    saveComment(patient, id) {
         return apiClient.post('/comment', {
             id: id,
             doctor_comm: patient.comment,
         })
     },
-  saveDtoP(doctor, id){
-      return apiClient.post('/savedtop', {
-        id: id,
-        doctor: doctor,
-      })
-  },
-  saveVac(vaccine, id){
-    return apiClient.post('/savevac', {
-      id: id,
-      vaccine: vaccine,
-    })
-  }
+    saveDtoP(doctor, id) {
+        return apiClient.post('/savedtop', {
+            id: id,
+            doctor: doctor,
+        })
+    },
+    saveVac(vaccine, id) {
+        return apiClient.post('/savevac', {
+            id: id,
+            vaccine: vaccine,
+        })
+    },
+    addVac(vaccine) {
+        return apiClient.post('/addvac', vaccine)
+    }
 }
--- src/views/admin/AdminVacSet.vue
+++ src/views/admin/AdminVacSet.vue
@@ -4,71 +4,88 @@
     <p>{{ Store.patients.surname }}</p>
 
     <form @submit.prevent="saveVaccine">
-        <h3>Add Vaccine</h3>
-
-        <select :value="modelValue"
-                v-bind="{
-      ...$attrs,
-      onChange: ($vaccine) => {
-        $emit('update:modelValue', $vaccine.target.value)
-      }
-    }"
-                id="vaccine[name]" name="vaccine[name]">
-          <option :value="Astraceneca" >Astraceneca</option>
-          <option :value="Sinovac" >Sinovac</option>
-          <option :value="Sinopharm" >Sinopharm</option>
-          <option :value="Moderna" >Moderna</option>
-        </select>
-        <button type="submit">Submit</button>
+      <h3>Add Vaccine</h3>
+      <label>Select Vaccine</label>
+      <select
+        :value="modelValue.name"
+        v-model="vaccine.name"
+        v-bind="{
+          ...$attrs,
+          onChange: ($vaccine) => {
+            $emit('update:modelValue', $vaccine.target.value)
+          }
+        }"
+        label="Select Vaccine"
+      >
+        <option value="Astraceneca">Astraceneca</option>
+        <option value="Sinovac">Sinovac</option>
+        <option value="Sinopharm">Sinopharm</option>
+        <option value="Moderna">Moderna</option>
+      </select>
+      <br />
+      <input type="text" placeholder="Input date ejected..." v-model="vaccine.date_injected" />
+      <br />
+      <button type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
 import DatabaseService from '@/services/DatabaseService.js'
-import Store from "@/store";
+import Store from '@/store'
 export default {
-    props: {
-      modelValue: {
-        type: [String, Number, Object],
-        default: ''
-      }
-    },
-inject: ['Store'],
+  props: {
+    modelValue: {
+      type: [String, Number, Object],
+      default: ''
+    }
+  },
+  inject: ['Store'],
   data() {
     return {
-        vaccine: [{
-            name:'', date_injected:''
-        }],
-        patient: Store.patients.id
-    };
+      vaccine: 
+        {
+          name: '',
+          date_injected: '',
+          patient: Store.patients
+        }
+    }
   },
   // eslint-disable-next-line no-unused-vars
   created() {
     DatabaseService.getVaccines()
       .then((response) => {
-        Store.vaccines = response.data;
+        Store.vaccines = response.data
       })
       .catch((error) => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   },
   methods: {
-      saveVaccine() {
-          DatabaseService.saveVac( this.vaccine, Store.patients.id)
-          .then((response) => {
-            console.log(response)
-            this.$router.push({
-              name: 'Home'
-            })
+    saveVaccine() {
+      console.log(this.vaccine)
+      DatabaseService.addVac(this.vaccine)
+      .then((response) => {
+          console.log(response)
+          this.$router.push({
+            name: 'Home'
           })
-          .catch((error) => {
-            console.log(error);
-        });
-      }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      // DatabaseService.saveVac(this.vaccine, Store.patients.id)
+      //   .then((response) => {
+      //     console.log(response)
+      //     this.$router.push({
+      //       name: 'Home'
+      //     })
+      //   })
+      //   .catch((error) => {
+      //     console.log(error)
+      //   })
+    }
   }
-
-
 }
 </script>
 
