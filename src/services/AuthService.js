8bf1c478f57f67b262d14bde3749c819cbe75b11
Kitsada <kitsada_t@cmu.ac.th>
2021-10-27 05:57:05 +0700

    fix2

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,74 +2,25 @@ import apiClient from '@/services/AxiosClient.js'
 import Store from '@/store'
 
 export default {
-  login(user) {
-    return apiClient
-      .post('/auth', {
-        username: user.username,
-        password: user.password
-      })
-      .then((response) => {
-        localStorage.setItem('token', response.data.token)
-        if (JSON.stringify(response.data.user) != null) {
-          localStorage.setItem('user', JSON.stringify(response.data.user))
-          Store.currentUser = response.data.user
-        } else {
-          localStorage.setItem('lowuser', user.username)
-          Store.currentLowUser = user.username
-        }
-    },
-    register(user) {
-        return apiClient.post('/register', {
-                username: user.username,
-                password: user.password,
-                email: user.email
-            })
-            
-    },
-    saveDoc(user) {
-        return apiClient.post('/registerdoc', {
-                username: user.username,
-                email: user.email,
-                password: user.password,
-                firstname: user.firstname,
-                lastname: user.lastname,
-                age : user.age
-            })
-            .then((response) => {
-                localStorage.setItem('token', response.data.token)
+    login(user) {
+        return apiClient.post('/auth', {
+            username: user.username,
+            password: user.password
+        }).then((response) => {
+            localStorage.setItem('token', response.data.token)
+            if (JSON.stringify(response.data.user) != null) {
                 localStorage.setItem('user', JSON.stringify(response.data.user))
                 GStore.currentUser = response.data.user
-                return Promise.resolve(response.data)
-            })
-            .catch((error) => {
-                return Promise.reject(error)
-            })
-    },
-    savePat(user) {
-        return apiClient.post('/registerpat', {
-                username: user.username,
-                email: user.email,
-                password: user.password,
-                firstname: user.firstname,
-                lastname: user.lastname,
-                age: user.age,
-                hometown: user.hometown
-            })
-            .then((response) => {
-                localStorage.setItem('token', response.data.token)
-                localStorage.setItem('user', JSON.stringify(response.data.user))
-                GStore.currentUser = response.data.user
-                return Promise.resolve(response.data)
-            })
-            .catch((error) => {
-                return Promise.reject(error)
-            })
-        return Promise.resolve(response.data)
-      })
-      .catch((error) => {
-        return Promise.reject(error)
-      })
-  },
+            } else {
+                localStorage.setItem('lowuser', user.username)
+                GStore.currentLowUser = user.username
+            }
+            return Promise.resolve(response.data)
+        }).catch((error) => {
+            return Promise.reject(error)
+        })
+
+},
   logout() {
     localStorage.removeItem('token')
     localStorage.removeItem('user')
